eb07023f7ae7418cfeca2b06104cd795fd5bcb3e
Yuheng Chen <andy-chen@berkeley.edu>
2022-07-28 18:22:23 +0100

    trees

--- validateBST.cpp
+++ validateBST.cpp
@@ -13,12 +13,14 @@ int main() {
 	*/
 	TreeNode* root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
 	function<bool(TreeNode*, int, int)> valid = [&] (TreeNode* root, int lo, int hi) {
-		db(lo, hi);
-		if (root == nullptr) return true;
 		if (!(lo <= root->val && root->val <= hi)) return false;
-		lo = max(root->val, lo);
-		hi = min(root->val, hi);
-		return valid(root->left, lo, root->val) && valid(root->right, root->val, hi);
+		bool yes = true;
+		if (root->left != nullptr) {
+			yes &= valid(root->left, max(root->left->val, lo), root->val);
+		}
+		if (root->right != nullptr) {
+			yes &= valid(root->right, root->val, min(root->right->val, hi));
+		}
 	};
 	bool res = valid(root, INT_MIN, INT_MAX);
 	printf("%d\n", res);
